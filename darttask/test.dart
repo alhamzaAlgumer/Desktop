3ab9297f30a742f1ae87ec23beaafb142891a9be
name <email@email.com>
2022-11-01 16:35:36 +0300

    task3 solve2

--- darttask/test.dart
+++ darttask/test.dart
@@ -1,20 +1,26 @@
 import 'dart:io';
 
-void main(){
-  List enterStudent=["ali","ahmed","mohamed"];
-  List leavestudent=["abdullah","khaled","hussein"];
+void main(){ print("to find enter a student to the class enter  1");
+        print("to find leave a student to the class enter  2");
+        print("to find all student enter 3");
+  //List enterStudent=["ali","ahmed","mohamed"];
+ // List leavestudent=["abdullah","khaled","hussein"];
   while(true){
+   
+
   var option=stdin.readLineSync();
+  
   if(option=="3"){
 interStudent();
 leaveStudent();
-break;
 
   }else if(option=="1"){
     interStudent();
 
   }else if(option=="2") {
   leaveStudent();
+  }else if(option=="4"){
+   break;
   }else{
     print("please enter 1 2 3 4");
     continue;
@@ -24,12 +30,12 @@ break;
 }
 void interStudent(){
     List enterStudent=["ali","ahmed","mohamed"];
-    print(enterStudent);
+    print("enter a student to the class $enterStudent");
 
 }
 void leaveStudent(){
     List leavestudent=["abdullah","khaled","hussein"];
-        print(leavestudent);
+        print("leave a student to the class $leavestudent");
 
 
 
